8bfc0b8b2f687b9c5a79ad18e785b3c5b41b55d5
Kshitij Judah <kjudah2004@gmail.com>
2015-11-09 23:20:49 -0800

    Use weka's Evaluation class to perform 10-fold CV evaluation of linear regression model.

--- src/main/scala/com/indeed/salary_prediction/TrainWekaLinearRegression.java
+++ src/main/scala/com/indeed/salary_prediction/TrainWekaLinearRegression.java
@@ -42,61 +42,8 @@ public class TrainWekaLinearRegression {
     System.out.println("10-Fold CV Mean Absolute Error = " + Double.toString(eval.meanAbsoluteError()));
     System.out.println("10-Fold CV Root Mean Squared Error = " + Double.toString(eval.rootMeanSquaredError()));
 
-//    int folds = 10;
-//    double sumCorrCV = 0.0;
-//    double sumMAECV = 0.0;
-//    double sumRMSECV = 0.0;
-//    for (int fold = 0; fold < folds; fold++) {
-//      System.out.println("Running Fold: " + fold);
-//      int seed = fold+1;  // every run gets a new, but defined seed value
-//      Random rand = new Random(seed);   // create seeded number generator
-//      Instances randData = new Instances(data);   // create copy of original data
-//      randData.randomize(rand);         // randomize data with number generator
-//
-//      Instances train = randData.trainCV(folds, fold);
-//      Instances test = randData.testCV(folds, fold);
-//
-//      // Building the model
-//      LinearRegression lr = new LinearRegression();
-//      SelectedTag tag = new SelectedTag(LinearRegression.SELECTION_NONE, LinearRegression.TAGS_SELECTION);
-//      lr.setAttributeSelectionMethod(tag); // turn off attribute selection
-//      lr.setEliminateColinearAttributes(false);
-//      lr.buildClassifier(train);
-//
-//      // Evaluate model on validation examples and compute validation error
-//      double sumMSE = 0.0;
-//      double sumMAE = 0.0;
-//      double[] actuals = new double[data.numInstances()];
-//      double[] preds = new double[data.numInstances()];
-//      for (int j=0; j < test.numInstances(); j++){
-//        Instance inst = test.instance(j);
-//        double prediction = lr.classifyInstance(inst);
-//        preds[fold] = prediction;
-//        double actual = inst.classValue();
-//        actuals[fold] = actual;
-//        sumMSE += Math.pow((prediction-actual), 2);
-//        sumMAE += Math.abs(prediction-actual);
-//      }
-//      double MSE = (1.*sumMSE)/data.numInstances();
-//      double RMSE = Math.sqrt(MSE);
-//      sumRMSECV += RMSE;
-//      double MAE = (1.*sumMAE)/data.numInstances();
-//      sumMAECV += MAE;
-//      // Compute correlation coefficient between actual and predictions
-//      PearsonsCorrelation pearsonsCorrelation = new PearsonsCorrelation();
-//      double correlation = pearsonsCorrelation.correlation(actuals, preds);
-//      sumCorrCV += correlation;
-//      System.out.println("Fold " + fold + " Correlation Coefficient = " + correlation);
-//      System.out.println("Fold " + fold + " Mean Absolute Error = " + MAE);
-//      System.out.println("Fold " + fold + " Root Mean Squared Error = " + RMSE);
-//    }
-//    double correlation = sumCorrCV/folds;
-//    double MAE = sumMAECV/folds;
-//    double RMSE = sumRMSECV/folds;
-//    System.out.println("10-Fold CV Correlation Coefficient = " + correlation);
-//    System.out.println("10-Fold CV Mean Absolute Error = " + MAE);
-//    System.out.println("10-Fold CV Root Mean Squared Error = " + RMSE);
-
+    // Train model on entire data
+    
     // Output model
 //    System.out.println(lr.toString());
   }
