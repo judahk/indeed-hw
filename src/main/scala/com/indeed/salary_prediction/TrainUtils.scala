98d52c94abef2e7afa14dabe390c042b7b690df7
Kshitij Judah <kjudah2004@gmail.com>
2015-11-12 11:55:56 -0800

    Added functions to load datasets.

--- src/main/scala/com/indeed/salary_prediction/TrainUtils.scala
+++ src/main/scala/com/indeed/salary_prediction/TrainUtils.scala
@@ -9,6 +9,137 @@ import org.apache.spark.rdd.RDD
  * Created by judahk on 11/9/2015.
  */
 object TrainUtils {
+  def loadTrainingData(dataFile: String, labelFile: String, sc: SparkContext) : RDD[(String, LabeledPoint)] = {
+    val featureData = loadFeatureData(dataFile, sc)
+    val labels = loadLabelData(labelFile, sc)
+
+    // Change nominal features to numeric via dummy variables
+    var dummyFeats = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[String, Int]]()
+    var dummyFeatsCount = scala.collection.mutable.Map[Int, Int]()
+    var totalDummyVars = 0 // count how many dummy vars we are introducing
+    for (indx <- 0 to 4){ //try to remove hardcoded indices
+      val vals = featureData.map{case(_,array) => array(indx)}
+      val uniqueVals = vals.distinct().collect() // distinct values can be collected as they are not very large often
+      val levels = uniqueVals.length
+      val numDummyFeats = levels - 1
+      val uniqueValsWithIds = uniqueVals.zipWithIndex
+      var indices = scala.collection.mutable.Map[String, Int]()
+      uniqueValsWithIds.foreach(t => indices += (t._1 -> t._2.toInt))
+      dummyFeats += (indx -> indices)
+      dummyFeatsCount += (indx -> numDummyFeats)
+      totalDummyVars = totalDummyVars + numDummyFeats
+    }
+    val newData = featureData.map{case(jobID, dataArr) =>
+      val array = dataArr.zipWithIndex.flatMap{t => t._2 match {
+        case indx if (0 <= indx && indx <= 4) =>
+          var arr = new Array[Double](dummyFeatsCount(indx))
+          val v = dummyFeats(indx)(dataArr(indx))
+          if (v != 0) arr(v-1) = 1.0
+          arr
+        case _ =>
+          var arr = new Array[Double](1)
+          arr(0) = t._1.toDouble
+          arr
+      }
+      }
+      (jobID, array)
+    }
+    //    newData.take(10).foreach{array => array.foreach(x => print(x + " ")) // generally do take(n) instead of collect to see RDD data (to avoid out of memory issues on driver machine)!!
+    //      println()
+    //    }
+    val labelData = labels.map{case(jobID,label) => (jobID,label.toDouble)}
+    val data = newData.join(labelData)
+    // Create training data (RDD of LabeledPoint)
+    val trainingData = data.map {case(jobID,(array,label)) =>
+      (jobID, LabeledPoint(label, Vectors.dense(array)))
+    }
+    trainingData
+  }
+
+  def loadTestData(dataFile: String, sc: SparkContext) : RDD[(String, LabeledPoint)] = {
+    val featureData = loadFeatureData(dataFile, sc)
+
+    // Change nominal features to numeric via dummy variables
+    var dummyFeats = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[String, Int]]()
+    var dummyFeatsCount = scala.collection.mutable.Map[Int, Int]()
+    var totalDummyVars = 0 // count how many dummy vars we are introducing
+    for (indx <- 0 to 4){ //try to remove hardcoded indices
+      val vals = featureData.map{case(_,array) => array(indx)}
+      val uniqueVals = vals.distinct().collect() // distinct values can be collected as they are not very large often
+      val levels = uniqueVals.length
+      val numDummyFeats = levels - 1
+      val uniqueValsWithIds = uniqueVals.zipWithIndex
+      var indices = scala.collection.mutable.Map[String, Int]()
+      uniqueValsWithIds.foreach(t => indices += (t._1 -> t._2.toInt))
+      dummyFeats += (indx -> indices)
+      dummyFeatsCount += (indx -> numDummyFeats)
+      totalDummyVars = totalDummyVars + numDummyFeats
+    }
+    val newData = featureData.map{case(jobID, dataArr) =>
+      val array = dataArr.zipWithIndex.flatMap{t => t._2 match {
+        case indx if (0 <= indx && indx <= 4) =>
+          var arr = new Array[Double](dummyFeatsCount(indx))
+          val v = dummyFeats(indx)(dataArr(indx))
+          if (v != 0) arr(v-1) = 1.0
+          arr
+        case _ =>
+          var arr = new Array[Double](1)
+          arr(0) = t._1.toDouble
+          arr
+      }
+      }
+      (jobID, array)
+    }
+    //    newData.take(10).foreach{array => array.foreach(x => print(x + " ")) // generally do take(n) instead of collect to see RDD data (to avoid out of memory issues on driver machine)!!
+    //      println()
+    //    }
+
+    // Create test data (RDD of LabeledPoint)
+    val testData = newData.map {case(jobID,array) =>
+      (jobID, LabeledPoint(0.0, Vectors.dense(array)))
+    }
+    testData
+  }
+
+  def loadFeatureData(dataFile: String, sc: SparkContext) : RDD[(String, Array[String])] = {
+    val data = sc.textFile(dataFile)
+    // Skip header line
+    val dataNoHeader = data.mapPartitionsWithIndex {(i, iterator) =>
+      if (i == 0 && iterator.hasNext){
+        iterator.next()
+        iterator
+      }else
+        iterator
+    }
+    val newData = dataNoHeader.map{l =>
+      val col = l.split(",")
+      val jobID = col(0)
+      val len = col.length
+      val rest = col.takeRight(len-1)
+      (jobID, rest)
+    }
+    newData
+  }
+
+  def loadLabelData(dataFile: String, sc: SparkContext) : RDD[(String, String)] = {
+    val data = sc.textFile(dataFile)
+    // Skip header line
+    val dataNoHeader = data.mapPartitionsWithIndex {(i, iterator) =>
+      if (i == 0 && iterator.hasNext){
+        iterator.next()
+        iterator
+      }else
+        iterator
+    }
+    val newData = dataNoHeader.map{l =>
+      val col = l.split(",")
+      val jobID = col(0)
+      val label = col(1)
+      (jobID, label)
+    }
+    newData
+  }
+
   def createKFoldCVSplits(data: RDD[LabeledPoint], numFolds: Int, sc: SparkContext) : Array[(RDD[LabeledPoint],RDD[LabeledPoint])] = {
     val wgt = 1.0/numFolds
     val wgts: Array[Double] = new Array[Double](numFolds)
@@ -67,6 +198,7 @@ object TrainUtils {
 
   def main(args: Array[String]) {
     val dataFile = args(0)
+    val labelFile = args(1)
 
     // Create SPARK conf and context
     val conf = new SparkConf().setAppName("CrossValidationTest").setMaster("local[*]").set("spark.local.dir", "C:\\tmp")
@@ -75,62 +207,10 @@ object TrainUtils {
     val sc = new SparkContext(conf)
 
     // Load and modify the data to appropriate form for learning
-    val data = sc.textFile(dataFile)
-
-    // Skip header line
-    val dataNoHeader = data.mapPartitionsWithIndex {(i, iterator) =>
-      if (i == 0 && iterator.hasNext){
-        iterator.next()
-        iterator
-      }else
-        iterator
-    }
-
-    // Remove job_id field as it is not predictive
-    val dataNoJobId = dataNoHeader.map{l =>
-      val col = l.split(",")
-      val len = col.length
-      val rest = col.takeRight(len-1)
-      rest
-    }
-
-    // Change nominal features to numeric via dummy variables
-    var dummyFeats = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[String, Int]]()
-    var dummyFeatsCount = scala.collection.mutable.Map[Int, Int]()
-    var totalDummyVars = 0 // count how many dummy vars we are introducing
-    for (indx <- 0 to 4){ //try to remove hardcoded indices
-    val vals = dataNoJobId.map(array => array(indx))
-      val uniqueVals = vals.distinct().collect() // distinct values can be collected as they are not very large often
-      val levels = uniqueVals.length
-      val numDummyFeats = levels - 1
-      val uniqueValsWithIds = uniqueVals.zipWithIndex
-      var indices = scala.collection.mutable.Map[String, Int]()
-      uniqueValsWithIds.foreach(args => indices += (args._1 -> args._2.toInt))
-      dummyFeats += (indx -> indices)
-      dummyFeatsCount += (indx -> numDummyFeats)
-      totalDummyVars = totalDummyVars + numDummyFeats
-    }
-    val newData = dataNoJobId.map{dataArr =>
-      dataArr.zipWithIndex.flatMap{args => args._2 match {
-        case indx if (0 <= indx && indx <= 4) =>
-          var arr = new Array[Double](dummyFeatsCount(indx))
-          val v = dummyFeats(indx)(dataArr(indx))
-          if (v != 0) arr(v-1) = 1.0
-          arr
-        case _ =>
-          var arr = new Array[Double](1)
-          arr(0) = args._1.toDouble
-          arr
-      }
-      }
-    }
-    //    newData.take(10).foreach{array => array.foreach(x => print(x + " ")) // generally do take(n) instead of collect to see RDD data (to avoid out of memory issues on driver machine)!!
-    //      println()
-    //    }
-
-    val trainingData = newData.map { array =>
-      LabeledPoint(array(array.length-1), Vectors.dense(array.take(array.length-1)))
-    }.cache()
+    println("Loading and creating training data")
+    val trainingDataWithJobIDs = loadTrainingData(dataFile, labelFile, sc)
+    val trainingData = trainingDataWithJobIDs.map(t => t._2).cache()
+    println("Done loading and creating training data")
 
     val k = 10
     val splits = TrainUtils.createKFoldCVSplits(trainingData, k, sc)
